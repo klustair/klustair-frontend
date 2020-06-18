0d23b572113732d9de2df6a0241487acd0fffcf3
mms-gianni <gianni.carafa@srf.ch>
2020-06-18 22:43:02 +0200

    add images view

--- resources/views/images.blade.php
+++ resources/views/images.blade.php
@@ -8,4 +8,60 @@
 
 @section('content')
     <p>Welcome to this beautiful admin panel.</p>
+    <div class="row">
+          <div class="col-4">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Reports</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0">
+<div class="list-group" id="myList" role="tablist">
+@foreach ($reports as $report)
+    <a class="list-group-item list-group-item-action" data-toggle="list" href="#home" role="tab">{{ $report->toDateTime()->format('r') }}</a>
+@endforeach
+</div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+
+          <div class="col-4">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Pods</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0">
+              <div class="list-group" id="myList" role="tablist">
+@foreach ($pods as $pod)
+    <a class="list-group-item list-group-item-action" data-toggle="list" href="#home" role="tab">{{$pod}}</a>
+@endforeach
+</div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+
+          <div class="col-4">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Images</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0">
+              <div class="list-group" id="myList" role="tablist">
+@foreach ($images as $image)
+    <a class="list-group-item list-group-item-action" data-toggle="list" href="#home" role="tab">{{$image}}</a>
+@endforeach
+</div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          
+        </div>
 @stop
