529d76bd3f1d7bbb88c6412bf6dbe9ede1ca2d45
mms-gianni <gianni.carafa@srf.ch>
2020-06-18 22:44:13 +0200

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Jenssegers\Mongodb\Query\Builder;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,9 +19,99 @@ Route::get('/', function () {
 });
 
 Route::get('/images', function () {
-    return view('images');
+
+/*
+
+    $klustair_all = DB::collection('pods')
+        ->get();
+    print_r($klustair_all);
+
+    $klustair_all = DB::collection('pods')
+        ->first();
+    print_r($klustair_all);
+
+    $klustair_namespace = DB::collection('pods')
+        ->where('metadata.namespace', 'cms-search')
+        ->first();
+    print_r($klustair_namespace);
+
+    $data['images'] = DB::collection('pods')
+        ->select("metadata.namespace","containers.image")
+        ->get();
+    print_r($data);
+*/
+    $data['pods'] = DB::collection('pods')
+        ->distinct("metadata.name")
+        ->get();
+
+    $data['images'] = DB::collection('pods')
+        ->distinct("containers.0.image")
+        ->get();
+
+    $data['reports'] = DB::collection('pods')
+        ->distinct("checktime")
+        ->get();
+    
+
+    return view('images', $data);
 });
 
-Route::get('/reports', function () {
-    return view('reports');
+Route::get('/latestreport', function () {
+
+    $data['checktime'] = DB::collection('pods')
+        ->distinct("checktime")
+        ->first();
+    $data['pods'] = DB::collection('pods')
+        //->select("metadata.namespace","containers.image")
+        ->where('checktime', $data['checktime'])
+        ->get();
+        //->first();
+        //print_r($data);
+
+    $vulnseverity = array(
+                    "High" => 'bg-danger text-white',
+                    "Critical" => 'bg-warning text-dark',
+                    "Medium" => 'bg-info text-white',
+                    "Low" => 'bg-secondary text-white',
+                    "Negligible" => 'bg-dark text-white',
+                    "Unknown" => 'bg-white text-dark'
+                );
+
+    $error = array(
+            "danger",
+            "success",
+        );
+        
+    $data['vulnseverity'] = $vulnseverity;
+    $data['error'] = $error;
+
+    return view('reports', $data);
+});
+
+
+
+Route::get('/pod/{podid}', function ($podid) {
+
+    $vulnseverity = array(
+                    "High" => 'bg-danger text-white',
+                    "Critical" => 'bg-warning text-dark',
+                    "Medium" => 'bg-info text-white',
+                    "Low" => 'bg-secondary text-white',
+                    "Negligible" => 'bg-dark text-white',
+                    "Unknown" => 'bg-white text-dark'
+                );
+
+    $error = array(
+            "danger",
+            "success",
+        );
+        
+    $data['vulnseverity'] = $vulnseverity;
+    $data['error'] = $error;
+
+    $data['pod'] = DB::collection('pods')
+        ->where('metadata.uid', $podid)
+        ->first();
+    $data['podid'] = $podid;
+    return view('pod', $data);
 });
