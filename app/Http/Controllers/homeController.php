34ad62823ede580bbf4bd0944bd8447b291e5732
mms-gianni <gianni.carafa@srf.ch>
2020-09-08 11:31:46 +0200

    Add Anchore API URL env var

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -16,21 +16,22 @@ class homeController extends Controller
     public function home() 
     {
         $anchore_api = Http::withBasicAuth('admin', 'foobar');
+        $anchore_api_url = env('ANCHORE_API_URL', 'http://host.docker.internal:8228');
         
-        $status = $anchore_api->get('http://host.docker.internal:8228/v1/system');
+        $status = $anchore_api->get("${anchore_api_url}/v1/system");
         $data['anchore_status'] = $status->json();
 
         $data['reports_count'] = DB::table('k_reports')
             ->distinct('uid')
             ->count();
 
-        $status = $anchore_api->get('http://host.docker.internal:8228/v1/images');
+        $status = $anchore_api->get("${anchore_api_url}/v1/images");
         $data['anchore_images'] = $status->json();
 
-        $status = $anchore_api->get('http://host.docker.internal:8228/v1/policies');
+        $status = $anchore_api->get("${anchore_api_url}/v1/policies");
         $data['anchore_policies'] = $status->json();
 
-        $data['anchore_feeds'] = $anchore_api->get('http://host.docker.internal:8228/v1/system/feeds')->json();
+        $data['anchore_feeds'] = $anchore_api->get("${anchore_api_url}/v1/system/feeds")->json();
         return view('home', $data);
     }
 }
