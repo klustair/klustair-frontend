e187f815d49362d2633432263d5683c42caeb2ca
mms-gianni <gianni.carafa@srf.ch>
2020-09-14 22:52:54 +0200

    Shift colors of cards when in different states

--- resources/views/anchore/images.blade.php
+++ resources/views/anchore/images.blade.php
@@ -14,29 +14,34 @@
 
             @foreach ($images as $image)
             <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch">
-              <div class="card bg-light">
-                <div class="card-header text-muted border-bottom-0">
 
-                @switch($image['analysis_status'])
-                  @case('not_analyzed')
-                    <i class="fas fa-lg fa-question-circle text-info"></i> {{ $image['analysis_status'] }}
-                    @break
-                  @case('analysing')
-                    <i class="fas fa-lg fa-running"></i> {{ $image['analysis_status'] }}
-                    @break
-                  @case('analyzed')
-                    <i class="fas fa-lg fa-check-circle text-success"></i> {{ $image['analysis_status'] }}
-                    @break
-                  @case('analysis_failed')
-                    <i class="fas fa-lg fa-exclamation-circle text-danger"></i> {{ $image['analysis_status'] }}
-                    @break
-                @endswitch
-                     
+              @switch($image['analysis_status'])
+                @case('not_analyzed')
+              <div class="card bg-light card-outline card-secondary">
+                <div class="card-header text-muted border-bottom-0">
+                  <i class="fas fa-lg fa-question-circle text-info"></i>
+                  @break
+                @case('analyzing')
+              <div class="card bg-light card-outline card-secondary">
+                <div class="card-header text-muted border-bottom-0">
+                  <i class="fas fa-lg fa-running"></i>
+                  @break
+                @case('analyzed')
+              <div class="card bg-light card-outline card-success">
+                <div class="card-header text-muted border-bottom-0">
+                  <i class="fas fa-lg fa-check-circle text-success" alt="analyzed"></i>
+                  @break
+                @case('analysis_failed')
+              <div class="card bg-light card-outline card-danger">
+                <div class="card-header text-muted border-bottom-0">
+                  <i class="fas fa-lg fa-exclamation-circle text-danger"></i>
+                  @break
+              @endswitch
+                <b>{{ $image['image_detail'][0]['repo'] }}:{{ $image['image_detail'][0]['tag'] }}</b>
                 </div>
                 <div class="card-body pt-0">
                   <div class="row">
                     <div class="col-10">
-                      <h2 class="lead"><b>{{ $image['image_detail'][0]['repo'] }}:{{ $image['image_detail'][0]['tag'] }}</b></h2>
                       <ul class="ml-4 mb-0 fa-ul text-muted">
                         <li class="small"><span class="fa-li"><i class="far fa-lg fa-folder"></i></span>registry: {{ $image['image_detail'][0]['registry'] }}</li>
                         <li class="small"><span class="fa-li"><i class="fas fa-lg fa-calendar-plus"></i></span>tag detected at: {{ $image['image_detail'][0]['tag_detected_at'] }}</li>
@@ -62,6 +67,11 @@
                     </a>
                   </div>
                 </div>
+                @if($image['analysis_status'] == 'analyzing')
+                <div class="overlay">
+                  <i class="fas fa-2x fa-sync-alt fa-spin"></i>
+                </div>
+                @endif
               </div>
             </div>
             @endforeach
