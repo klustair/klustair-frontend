631181775fd94898f938e22da29fe762f1d39e28
mms-gianni <gianni.carafa@srf.ch>
2020-09-14 21:38:32 +0200

    Show only newest image and remove older dublicates

--- app/Http/Controllers/Anchore/ImagesController.php
+++ app/Http/Controllers/Anchore/ImagesController.php
@@ -16,8 +16,30 @@ class ImagesController extends Controller
 
         $anchore_api = Http::withBasicAuth($anchore_api_user, $anchore_api_pass);
 
-        $status = $anchore_api->get("${anchore_api_url}/v1/images");
-        $data['images'] = $status->json();
+        $images = $anchore_api->get("${anchore_api_url}/v1/images")->json();
+
+        # this is the PHP implementation of this anchore_cli Code to show only newest version of image
+        # https://github.com/anchore/anchore-cli/blob/ef2108b20a5895c6ebdb26ec47ce53f39ef52adc/anchorecli/cli/utils.py#L251
+        $latest_tag_details = array();
+        $latest_records = array();
+        foreach ($images as $image_record) {
+            foreach ($image_record['image_detail'] as $image_detail) {
+                $fulltag = $image_detail['fulltag'];
+                $tagts = strtotime ( $image_detail['created_at'] );
+                if ( !array_key_exists($fulltag, $latest_tag_details) ){
+                    $latest_tag_details[$fulltag] = $image_detail;
+                    $latest_records[$fulltag] = $image_record;
+                } else {
+                    $lasttagts = strtotime($latest_tag_details[$fulltag]['created_at']);
+                    if ($tagts >= $lasttagts) {
+                        $latest_tag_details[$fulltag] = $image_detail;
+                        $latest_records[$fulltag] = $image_record;
+                    }
+                }
+            }
+        }
+
+        $data['images'] = $latest_records;
 
         /*
         echo "<pre>";
