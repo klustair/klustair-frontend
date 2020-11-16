551567b803c6681e426f51ccfe6a8018a6850ce4
mms-gianni <gianni.carafa@srf.ch>
2020-11-16 22:06:26 +0100

    open link in a new tab or window

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,8 +1,8 @@
 @section('footer')
 
 <div class="float-right d-none d-sm-inline-block">
-    <b><a href="https://hub.docker.com/repository/docker/klustair/klustair-frontend" style="color:gray">v0.2.4</a></b> 
+    <b><a href="https://hub.docker.com/repository/docker/klustair/klustair-frontend" target="_blank" style="color:gray">v0.2.4</a></b> 
 </div>
 
-<a href="https://github.com/klustair" style="color:gray">KlustAIR</a>
+<a href="https://github.com/klustair" target="_blank" style="color:gray">KlustAIR</a>
 @stop
