7f6cbd0e8ee61f96d7eecb00e5105e166e230fcd
mms-gianni <gianni.carafa@srf.ch>
2020-11-16 22:03:26 +0100

    add link to docker hub

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,7 +1,7 @@
 @section('footer')
 
 <div class="float-right d-none d-sm-inline-block">
-    <b>v0.2.4</b> 
+    <b><a href="https://hub.docker.com/repository/docker/klustair/klustair-frontend" style="color:gray">v0.2.4</a></b> 
 </div>
 
 <a href="https://github.com/klustair" style="color:gray">KlustAIR</a>
