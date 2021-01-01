a4a1c2445429be6a0841bb2861ff68f34e13b137
mms-gianni <gianni.carafa@srf.ch>
2021-01-01 18:11:57 +0100

    Create config runner table

---
+++ database/migrations/2021_01_01_10000o_create_config_runner.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConfigRunner extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $create_sql[] = <<<SQL
+            CREATE TABLE IF NOT EXISTS public.k_config_runner
+            (
+                uid character varying COLLATE pg_catalog."default" NOT NULL,
+                runner_label character varying COLLATE pg_catalog."default",
+                kubeaudit character varying COLLATE pg_catalog."default",
+                "verbose" boolean,
+                namespacesblacklist character varying COLLATE pg_catalog."default",
+                namespaces character varying COLLATE pg_catalog."default",
+                anchore boolean,
+                trivy boolean,
+                trivycredentialspath character varying COLLATE pg_catalog."default",
+                limit_date character varying COLLATE pg_catalog."default",
+                limit_nr integer,
+                CONSTRAINT k_runner_configs_pkey PRIMARY KEY (uid)
+            )
+        SQL;
+
+        $dbuser = env('DB_USERNAME', 'postgres');
+        $create_sql[] = <<<SQL
+            ALTER TABLE public.k_config_runner
+                OWNER to $dbuser;
+        SQL;
+
+        foreach ($create_sql as $sql ) {
+            DB::statement($sql);
+        }
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('k_config_runner');
+    }
+}
