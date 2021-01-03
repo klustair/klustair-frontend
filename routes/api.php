8ed320cd66d4779bd3e6598d00df99ff254a72be
mms-gianni <gianni.carafa@srf.ch>
2021-01-03 23:14:35 +0100

    Add sanctum

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
@@ -41,7 +41,7 @@ Route::prefix('v1')->group(function () {
 
 });
 
-Route::group(['prefix' => 'v1', 'middleware' => ['auth:api']], function(){
+Route::group(['prefix' => 'v1', 'middleware' => ['auth:sanctum']], function(){
     
     Route::get('/test', function () {
         return [1, 2, 3];
