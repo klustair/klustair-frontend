d297b62f30871f41ca3d16da6c55a9f28931b330
mms-gianni <gianni.carafa@srf.ch>
2021-01-28 15:13:27 +0100

    Bump Version

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,7 +1,7 @@
 @section('footer')
 
 <div class="float-right d-none d-sm-inline-block">
-    <b><a href="https://hub.docker.com/repository/docker/klustair/klustair-frontend" target="_blank" style="color:gray">v0.3.1</a></b> 
+    <b><a href="https://hub.docker.com/repository/docker/klustair/klustair-frontend" target="_blank" style="color:gray">v0.3.2</a></b> 
 </div>
 
 <a href="https://github.com/klustair" target="_blank" style="color:gray">KlustAIR</a>
