14c1d37c7483daab7535af4d9bf44586140d03a9
Gianni Carafa <gianni.carafa@srf.ch>
2021-07-24 17:23:04 +0200

    rename indizes

--- database/migrations/2021_07_12_100000_add_indexes2.php
+++ database/migrations/2021_07_12_100000_add_indexes2.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddIndexes extends Migration
+class AddIndexes2 extends Migration
 {
     /**
      * Run the migrations.
