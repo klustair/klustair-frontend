16aaa042110b5649816771e786056493efc84c03
Gianni Carafa <gianni.carafa@srf.ch>
2021-08-03 11:51:36 +0200

    fix is_os migration

--- database/migrations/2021_08_03_100000_add_target_is_os.php
+++ database/migrations/2021_08_03_100000_add_target_is_os.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddTargetOs extends Migration
+class AddTargetIsOs extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddTargetOs extends Migration
     public function up()
     {
         $create_sql[] = <<<SQL
-            ALTER TABLE public.k_target_trivy ADD COLUMN IF NOT EXISTS isOS boolean
+            ALTER TABLE public.k_target_trivy ADD COLUMN IF NOT EXISTS is_os boolean
         SQL;
         foreach ($create_sql as $sql ) {
             DB::statement($sql);
@@ -29,7 +29,7 @@ class AddTargetOs extends Migration
     public function down()
     {
         $create_sql[] = <<<SQL
-            ALTER TABLE public.k_target_trivy DROP COLUMN isOS;
+            ALTER TABLE public.k_target_trivy DROP COLUMN is_os;
         SQL;
 
         foreach ($create_sql as $sql ) {
